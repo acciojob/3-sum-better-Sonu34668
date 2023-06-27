623d5ab6cd7139f47f42a29984ffc6937d01ab4e
Sonu34668 <130197661+Sonu34668@users.noreply.github.com>
2023-06-27 06:20:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,17 +2,18 @@ function threeSum(arr, target) {
 let closestSum = Infinity;
 	let minDiff = Infinity;
 	arr.sort((a,b) => a-b);
-	for(let i=0; i<arr.length-2; i++){
-		let left=i+1;
+	
+	for(let i=0; i < arr.length-2; i++){
+		let left = i+1;
 		let right = arr.length - 1;
 		while(left<right){
 			const sum = arr[i]+arr[left] + arr[right];
 			const diff = Math.abs(sum - target);
-			if(diff<mindiff){
+			if(diff < minDiff){
 				minDiff=diff;
 				closestSum=sum;
 			}
-			if(sum<target){
+			if(sum < target){
 				left++;
 			}
 			else{
